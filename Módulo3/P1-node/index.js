1f4a8b20c8e597d1a6935dda4e7a32000a90da65
Jadley Felipe <jadleyd@gmail.com>
2022-09-03 13:30:46 -0300

    adicionando o P1 do modulo3, feito no pc de maria

---
+++ Módulo3/P1-node/index.js
@@ -0,0 +1,14 @@
+console.log("Exemplo - media")
+
+var nota1, nota2, media
+
+nota1 = 7
+console.log("\nSua primeira nota: " + nota1)
+nota2 = 9.5
+console.log("\nSua segunda nota: " + nota2)
+
+media = (nota1 + nota2)/2
+
+if(media >= 7){
+    console.log("\nVocê foi aprovado!\nCom média: " + media)
+}
